d7769741067c99bc0d402f45aebc1a77a832e24f
DavidKN10 <davidrs23178@gmail.com>
2025-08-27 21:33:01 -0500

    set up project

---
+++ SDL3-Game/src/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "Could Not Load" << std::endl;
+		return -1;	
+	}
+	else {	
+		std::cout << "Loaded Successfully!" << std::endl;
+		SDL_Quit();
+		return 0;	
+	}	
+}
