8414d7fb8fc2415d6947bc160eb215b39ae61cd7
DavidKN10 <davidrs23178@gmail.com>
2025-09-04 01:02:09 -0500

    added SDL_image, assets, and simple loading texture

--- SDL3-Game/src/main.cpp
+++ SDL3-Game/src/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 
 struct SDLState {
 	SDL_Window* window;
@@ -35,6 +36,10 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	// load game assets
+	SDL_Texture* idleTexture = IMG_LoadTexture(state.renderer, "SDL3-Game/src/assets/school_girl_idle.png");
+	SDL_SetTextureScaleMode(idleTexture, SDL_SCALEMODE_NEAREST);
+
 	// start game loop
 	bool running = true;
 	while (running) {
@@ -48,13 +53,30 @@ int main(int argc, char *argv[])
 		}
 
 		// perform drawing commands
-		SDL_SetRenderDrawColor(state.renderer, 255, 255, 255, 255);
+		SDL_SetRenderDrawColor(state.renderer, 20, 10, 30, 255);
 		SDL_RenderClear(state.renderer);
+
+		SDL_FRect src{
+			.x = 0,
+			.y = 0,
+			.w = 32,
+			.h = 80
+		};
+
+		SDL_FRect dst{
+			.x = 0,
+			.y = 0,
+			.w = 32,
+			.h = 80
+		};
+		
+		SDL_RenderTexture(state.renderer, idleTexture, &src, &dst);
 
 		// swap buffers and presents
 		SDL_RenderPresent(state.renderer);
 	}
-
+	
+	SDL_DestroyTexture(idleTexture);
 	cleanup(state);
 	return 0;
 }
